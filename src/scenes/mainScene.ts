0371416f278994828d49f7587951167d74ec8e29
Lizzy Kalfas <lizzykal@udel.edu>
2024-02-25 20:15:51 -0500

    Part 2

--- src/scenes/mainScene.ts
+++ src/scenes/mainScene.ts
@@ -2,6 +2,8 @@ import Phaser from "phaser";
 
 export default class MainScene extends Phaser.Scene {
     private platforms?: Phaser.Physics.Arcade.StaticGroup;
+    private player?: Phaser.Physics.Arcade.Sprite;
+    private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
 
     constructor() {
         super({ key: "MainScene" });
@@ -9,7 +11,6 @@ export default class MainScene extends Phaser.Scene {
 
     create() {
         this.add.image(400, 300, "sky");
-        this.add.image(300, 300, "star");
         this.platforms = this.physics.add.staticGroup();
         const ground = this.platforms.create(
             400,
@@ -22,6 +23,41 @@ export default class MainScene extends Phaser.Scene {
         this.platforms.create(600, 400, "platform");
         this.platforms.create(50, 250, "platform");
         this.platforms.create(750, 220, "platform");
+
+        this.player = this.physics.add.sprite(100, 450, "dude");
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: "left",
+            frames: this.anims.generateFrameNumbers("dude", {
+                start: 0,
+                end: 3,
+            }),
+            frameRate: 10,
+            repeat: -1,
+        });
+
+        this.anims.create({
+            key: "turn",
+            frames: [{ key: "dude", frame: 4 }],
+            frameRate: 20,
+        });
+
+        this.anims.create({
+            key: "right",
+            frames: this.anims.generateFrameNumbers("dude", {
+                start: 5,
+                end: 0,
+            }),
+            frameRate: 10,
+            repeat: -1,
+        });
+
+        this.physics.add.collider(this.player, this.platforms);
+
+        this.cursors = this.input.keyboard?.createCursorKeys();
+
         const message = `Phaser v${Phaser.VERSION}`;
         this.add
             .text(this.cameras.main.width - 15, 15, message, {
@@ -29,5 +65,21 @@ export default class MainScene extends Phaser.Scene {
                 fontSize: "24px",
             })
             .setOrigin(1, 0);
+    }
+
+    update() {
+        if (this.cursors?.left.isDown) {
+            this.player?.setVelocityX(-160);
+            this.player?.anims.play("left", true);
+        } else if (this.cursors?.right.isDown) {
+            this.player?.setVelocityX(160);
+            this.player?.anims.play("right", true);
+        } else {
+            this.player?.setVelocityX(0);
+            this.player?.anims.play("turn");
+        }
+        if (this.cursors?.up.isDown && this.player?.body?.touching.down) {
+            this.player.setVelocityY(-330);
+        }
     }
 }
